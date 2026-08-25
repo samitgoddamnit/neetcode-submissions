bf92ad6480565dfc8791b5da7cc6a73d53c54799
Samit Awal <55499565+samitgoddamnit@users.noreply.github.com>
2026-08-25 13:26:05 +0100

    Add: count-good-nodes-in-binary-tree - submission-6

---
+++ Data Structures & Algorithms/count-good-nodes-in-binary-tree/submission-6.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int goodNodes(TreeNode root) {
+        return dfs(root,root.val);
+    }
+
+    private int dfs(TreeNode node,Integer current_max){
+        //needed help with memory issues / queue issues. algorithm I got right on my own.
+        //used a priority queue to be able to store the max, but I think I could have just
+        //passed the max value itself.
+        if(node == null){
+            return 0;
+        }
+        
+        if(node.val >= current_max){
+            return dfs(node.left,Math.max(node.val,current_max)) + dfs(node.right,Math.max(node.val,current_max)) + 1;
+        }
+        return dfs(node.left,current_max) + dfs(node.right,current_max);
+    }
+}
