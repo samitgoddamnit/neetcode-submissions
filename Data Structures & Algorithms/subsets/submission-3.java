34f42796a1a684ea9e9349e25de1fc6e02ff990a
Samit Awal <55499565+samitgoddamnit@users.noreply.github.com>
2026-09-04 13:46:37 +0100

    Add: subsets - submission-3

---
+++ Data Structures & Algorithms/subsets/submission-3.java
@@ -0,0 +1,21 @@
+class Solution {
+    //didn't really implement a backtracking method. rather, used an iterative approach with a stack.
+    //going to implement the backtracking method now.
+    public List<List<Integer>> subsets(int[] nums) {
+        return dfs(0,nums,new ArrayList(),new ArrayList());
+    }
+
+    private List<List<Integer>> dfs(int i, int[] nums, List<Integer> subset, List<List<Integer>> res){
+        if(i == nums.length){
+            res.add(new ArrayList<>(subset));
+            return res;
+        }
+        subset.add(nums[i]);
+        i += 1;
+        res = dfs(i,nums,subset,res);
+        subset.remove(subset.size() - 1);
+        res = dfs(i,nums,subset,res);
+        return res;
+    }
+    
+}
